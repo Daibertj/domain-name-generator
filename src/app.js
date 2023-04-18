cdcbab81a71ae0bbf43eb16819cbaf4541ec9a48
Daibert Ramirez <daibertjrg@gmail.com>
2023-04-18 16:25:51 +0000

    Domain name generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let item1 of pronoun) {
+    for (let item2 of adj) {
+      for (let item3 of noun) {
+        console.log(`${item1}${item2}${item3}.com`);
+      }
+    }
+  }
 };
